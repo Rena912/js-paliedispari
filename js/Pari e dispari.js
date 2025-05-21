3f2b92f942907085bc3385779246016b7e50ade4
Rena912 <renatopirri@gmail.com>
2025-05-21 21:45:00 +0200

    end pari e dispari

--- js/Pari e dispari.js
+++ js/Pari e dispari.js
@@ -7,6 +7,27 @@ function getRandomBetween(min, max) {
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
 
+function pari(num) {
+    if (num % 2 === 0){
+        return true
+    } else {
+        return false
+    }
+}
+
+// const UserInput = prompt('Scegli pari o dispari ')
 const PCnumber = getRandomBetween(1,6)
-const IDnumber = parseInt (prompt('Inserisci un numero intero da 1 a 6'))
-console.log(IDnumber, PCnumber)
+// const IDnumber = parseInt (prompt('Inserisci un numero intero da 1 a 6'))
+
+
+const somma = IDnumber + PCnumber
+
+if (pari(somma) && UserInput === 'pari') {
+    console.log('hai vinto')
+} else if (UserInput === "dispari" && !pari(somma)) {
+    console.log('hai Vinto')
+} else {
+    console.log('Hai Perso')
+}
+
+console.log(somma)
