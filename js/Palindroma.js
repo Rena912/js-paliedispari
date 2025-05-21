2ef36b5d40bd0939cf9bee993bf2060ed2cc3a2c
Rena912 <renatopirri@gmail.com>
2025-05-21 21:22:19 +0200

    first commit

---
+++ js/Palindroma.js
@@ -0,0 +1,33 @@
+const parolaUtente = prompt('Inserisci una parola')
+console.log(invertiParola(parolaUtente))
+
+
+if (parolaPalindroma(parolaUtente)) {
+	console.log('La parola e palindroma')
+} else {
+	console.log('la parola non e palindroma')
+}
+
+function parolaPalindroma(parola) {
+	parola = parola.toLowerCase()
+	const parolaInvertita = invertiParola(parola)
+	// console.log(parolaInvertita, parola)
+	if (parolaInvertita === parola) {
+		return true
+	} else {
+		return false
+	}
+
+}
+
+function invertiParola(parola) {
+	let parolaInvertita = ''
+
+	for (let i = 0; i < parola.length; i++) {
+		const char = parola.at(-1 - i)
+		parolaInvertita += char
+	}
+
+	return parolaInvertita
+}
+
